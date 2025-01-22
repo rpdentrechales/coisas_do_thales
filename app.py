e996007821ff4eb28e58e79f4382e50bff5041f4
rpdentrechales <rpdprocorpo@gmail.com>
2025-01-22 19:08:48 +0000

    Updating app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+# --- PAGE SETUP ---
+page_1 = st.Page(
+    "views/page_1.py",
+    title="Exemplo MongoDB",
+    icon=":material/savings:",
+    default=True
+)
+
+page_2 = st.Page(
+    "views/page_2.py",
+    title="Página 2",
+    icon=":material/settings:",
+)
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Páginas": [page_1]
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+# st.logo("assets/codingisfun_logo.png")
+
+# --- RUN NAVIGATION ---
+pg.run()
---
+++ views/page_1.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from pymongo import MongoClient, UpdateOne
+import datetime
+
+st.set_page_config(page_title="Exemplo Requisição MongoDB com Filtros", page_icon="💰", layout="wide")
+
+# Função auxiliar para pegar os dados do MongoDB
+@st.cache_data
+def get_dataframe_from_mongodb(collection_name, database_name, query={}):
+
+    client = MongoClient(f"mongodb+srv://rpdprocorpo:iyiawsSCfCsuAzOb@cluster0.lu6ce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    db = client[database_name]
+    collection = db[collection_name]
+
+    data = list(collection.find(query))
+
+    if data:
+        dataframe = pd.DataFrame(data)
+        if '_id' in dataframe.columns:
+            dataframe = dataframe.drop(columns=['_id'])
+    else:
+        dataframe = pd.DataFrame()
+
+    return dataframe
+
+st.title("Exemplo Requisição MongoDB com Filtros")
+
+# Inicia o seletor de datas
+
+hoje = datetime.now()
+primeiro_dia_do_mes = hoje.replace(day=1)
+
+data_input = st.date_input(
+    "Selecione uma data",
+    (primeiro_dia_do_mes, hoje),
+    format="DD/MM/YYYY",
+)
+
+data_inicial = data_input[0]
+data_final = data_input[1]
+
+# Faz a query
+
+botao_fazer_query = st.button("Fazer Query")
+
+if botao_fazer_query:
+  query = {"date": {"$gte": data_inicial, "$lte": data_final}}
+  billcharges_df = get_dataframe_from_mongodb(collection_name="billcharges_db", database_name="dash_midia",query=query)
+
+  st.dataframe(billcharges_df)
